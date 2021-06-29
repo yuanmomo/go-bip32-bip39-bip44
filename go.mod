a539b48bc4293c3a0645229601a8f5e5cbae8648
袁 鸿彬 <hongbin.yuan@za.group>
2021-06-29 17:01:00 +0800

    add bip32 bip39 bip44

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module go-hd-wallet
 go 1.16
 
 require (
+	github.com/jbenet/go-base58 v0.0.0-20150317085156-6237cf65f3a6 // indirect
 	github.com/tyler-smith/go-bip32 v1.0.0 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 )
--- main/main.go
+++ main/main.go
@@ -3,16 +3,54 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/jbenet/go-base58"
 	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
 )
 
-func main()  {
-	extendedMasterPublic, _ := bip32.B58Deserialize("xpub6E3rJViLG4vuxAFtecHm7iTkbUvgudwH8CwYZkAp3z5GJxheVM1giUuSvvzmGA4Tzs9HbEd2skmqbwPtLNuNY6sKdYZvUCr8VdNduPsmYAT")
+func main() {
+	// Generate seed by BIP39 Mnemonic and BIP39 Passphrase
+	mnemonic := "purse alcohol glimpse oblige birth return urge gift innocent town now patient"
+	passphrase := ""
+	seed := bip39.NewSeed(mnemonic, passphrase)
+	fmt.Printf("mnemonic : %s\n", mnemonic)
+	fmt.Printf("Seed : %s\n", hex.EncodeToString(seed))
+	fmt.Println()
 
-	pubKey, _ := extendedMasterPublic.NewChildKey(0)
-	fmt.Printf("%s \n",hex.EncodeToString(pubKey.Key))
+	// TODO. handle error
+	// BIP32 Root Key
+	rootPrivateKey, _ := bip32.NewMasterKey(seed)
+	rootPubKey := rootPrivateKey.PublicKey()
+	fmt.Printf("BIP32 root private key : %s\n", rootPrivateKey.String())
+	fmt.Printf("BIP32 root public key : %s\n", rootPubKey.String())
+	fmt.Println()
 
+	// Get BIP44 Extended Private Key
+	m44H, _ := rootPrivateKey.NewChildKey(bip32.FirstHardenedChild + 44)
+	m44H60H, _ := m44H.NewChildKey(bip32.FirstHardenedChild + 60)
+	m44H60H0H, _ := m44H60H.NewChildKey(bip32.FirstHardenedChild + 0)
+	m44H60H0H0PrivateKey, _ := m44H60H0H.NewChildKey(0)
+	// Get BIP44 Extended public Key
+	m44H60H0H0PublicKey := m44H60H0H0PrivateKey.PublicKey()
+	fmt.Printf("BIP32/BIP44 extended private key : %s\n", m44H60H0H0PrivateKey.String())
+	fmt.Printf("BIP32/BIP44 extended public key : %s\n", m44H60H0H0PublicKey.String())
+	fmt.Println()
 
-	pubKey3, _ := extendedMasterPublic.NewChildKey(3)
-	fmt.Printf("%s \n",hex.EncodeToString(pubKey3.Key))
+	bip44ExtendedPrivateKey, _ := bip32.B58Deserialize(m44H60H0H0PrivateKey.String())
+	bip44ExtendedPublicKey, _ := bip32.B58Deserialize(m44H60H0H0PublicKey.String())
+
+	// Derivation Path m/44'/60'/0'/0:
+	for i := 0; i < 5; i++ {
+		// Get private key by BIP44 Extended Private Key
+		priKeyBIP44, _ := bip44ExtendedPrivateKey.NewChildKey(uint32(i))
+		priKeyDecode:= base58.Decode(priKeyBIP44.String())
+		privateKey := priKeyDecode[46:78]
+
+		// Get public key by BIP44 Extended Private Key
+		pubKey, _ := bip44ExtendedPublicKey.NewChildKey(uint32(i))
+
+		// print
+		fmt.Printf("Path : m/44'/60'/0'/0/%d, public key: 0x%s, private key : 0x%s\n",
+			i, hex.EncodeToString(pubKey.Key), hex.EncodeToString(privateKey))
+	}
 }
